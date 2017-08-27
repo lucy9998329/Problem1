cbf5a02429195382bcd946ac134efc8d41374f02
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-08-28 11:26:47 +1200

    for loop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          //Start the program with Clear();
+          Console.Clear();
+          
+          /*Console.WriteLine("This program will display a number that you type in");
+          var number = 0;
+          Console.WriteLine("Please type a number");
+          var isNumbr = int.TryParse(Console.ReadLine(), out number);
+          if (isNumbr){
+              Console.WriteLine($"The number is {number}");
+
+          }*/
+
+          int number = 0;
+          int counter = 10;
+
+
+
+          for (int i = 0; i < counter; i++){
+            
+            Console.WriteLine("Please enter a number");
+            int input = int.Parse(Console.ReadLine());
+
+            number += input;
+
+          }
+
+          Console.WriteLine($"Total is {number} ");
+          Console.WriteLine($"Avarage is {number / 10}");
+
+
+
+          
+          //End the program with blank line and instructions
+          Console.ResetColor();
+          Console.WriteLine();
+          Console.WriteLine("Press <Enter> to quit the program");
+          Console.ReadKey();
+
+            
         }
     }
 }
