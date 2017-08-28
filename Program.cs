05f54a924000ee8c4e1054c623badc49f89f421d
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-08-28 12:03:39 +1200

    bool

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace problem1
 
           }*/
 
-          int number = 0;
+          /*int number = 0;
           int counter = 10;
 
 
@@ -33,7 +33,33 @@ namespace problem1
           }
 
           Console.WriteLine($"Total is {number} ");
-          Console.WriteLine($"Avarage is {number / 10}");
+          Console.WriteLine($"Avarage is {number / 10}");*/
+
+          /*var number = 0;
+          var counter = 10;
+
+
+
+          for (int i = 0; i < counter; i++){
+            
+            Console.WriteLine("Please enter a number");
+            var isNumber = int.TryParse(Console.ReadLine(), out int input);
+
+            number += input;
+
+          }
+
+          Console.WriteLine($"Total is {number} ");
+          Console.WriteLine($"Avarage is {number / counter}");*/
+
+          var number = 0;
+          Console.WriteLine("Do you want to add a number?");
+          var answer = true;
+          var isNumber = bool.TryParse(Console.ReadLine(), out answer);
+
+          if (answer == true)
+
+
 
 
 
